1f49eea0d5eeba772f5079e7959e7b6393b8574b
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-09-07 12:02:22 +0100

    Swap pointers
    Shrink pointer on left hand, grow pointer on right

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,24 @@
 extends XROrigin3D
 
-@onready var _grow_pointer: XRToolsFunctionPointer = $LeftHand/GrowPointer
-@onready var _shrink_pointer: XRToolsFunctionPointer = $RightHand/ShrinkPointer
+@onready var _grow_pointer: XRToolsFunctionPointer = $RightHand/GrowPointer
+@onready var _shrink_pointer: XRToolsFunctionPointer = $LeftHand/ShrinkPointer
 
 
 func _on_left_hand_button_pressed(button: String) -> void:
 	if button == "trigger_click":
-		_grow_pointer.enabled = true
+		_shrink_pointer.enabled = true
 
 
 func _on_left_hand_button_released(button: String) -> void:
 	if button == "trigger_click":
-		_grow_pointer.enabled = false
+		_shrink_pointer.enabled = false
 
 
 func _on_right_hand_button_pressed(button: String) -> void:
 	if button == "trigger_click":
-		_shrink_pointer.enabled = true
+		_grow_pointer.enabled = true
 
 
 func _on_right_hand_button_released(button: String) -> void:
 	if button == "trigger_click":
-		_shrink_pointer.enabled = false
+		_grow_pointer.enabled = false
